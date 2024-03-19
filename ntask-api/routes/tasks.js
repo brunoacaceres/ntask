797056ead39c03ce4709f007529eabd0fabdbdc5
Bruno A. Caceres <brunocaceres@mpms.mp.br>
2024-03-19 19:00:07 -0400

    Implementando CRUD de tasks e users.

--- ntask-api/routes/tasks.js
+++ ntask-api/routes/tasks.js
@@ -1,12 +1,62 @@
 module.exports = app => {
   const Tasks = app.models.tasks;
 
-  app.get('/tasks', async (req, res) => {
-    try {
-      const tasks = await Tasks.findAll();      
-      res.json({ tasks });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  app.route('/tasks')
+    .get(async (req, res) => {
+      //	"/tasks":	Lista	tarefas
+      try {
+        const result = await Tasks.findAll();
+        res.json(result);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    })
+    .post(async (req, res) => {
+      //	"/tasks":	Cadastra	uma	nova	tarefa
+      try {
+        const result = await Tasks.create(req.body);
+        res.json(result);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    });
+  app.route('/tasks/:id')
+    .get(async (req, res) => {
+      //	"/tasks/1":	Consulta uma	tarefa
+      try {
+        const { id } = req.params;
+        const where = { id }
+        const result = await Tasks.findOne({ where })
+        if (result) {
+          res.json(result);
+        } else {
+          res.sendStatus(404);
+        }
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    })
+    .put(async (req, res) => {
+      //	"/tasks/1":	Atualiza	uma	tarefa
+      try {
+        const { id } = req.params;
+        const where = { id };
+        await Tasks.update(req.body, { where });
+        res.sendStatus(204);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    })
+    .delete(async (req, res) => {
+      //"/tasks/1":	Exclui uma	tarefa
+      try {
+        const { id } = req.params;
+        const where = { id };
+        await Tasks.destroy({ where })
+        res.sendStatus(204);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    });
+
 };
